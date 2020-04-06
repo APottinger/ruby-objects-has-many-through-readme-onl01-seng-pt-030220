512677a3e488fdacd298b1fc8efb4ddbd8871d89
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-06 16:46:22 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,13 @@
 class Waiter
-
+  @@all = []
+  
+  def self.all 
+    @@all
+  end 
+  
+  attr_accessor :name, :yrs_of_exp
+  
+  def initialize(name, yrs_of_exp)
+    @name, @yrs_of_exp = name, yrs_of_exp
+  end 
 end
