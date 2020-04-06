af2203fad050d44da7ea645329dcfa3b5d40337d
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-06 16:45:02 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,15 @@
 class Customer
   
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  attr_accessor :name, :age 
+  
+  def initialize(name, age)
+    @name, @age = name, age
+  end 
+  
 end
