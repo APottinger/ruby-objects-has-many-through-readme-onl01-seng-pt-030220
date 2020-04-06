ff6e5bbc0da29919bb1a647e8f4caddcfc745a63
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-06 17:47:08 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -22,9 +22,11 @@ class Waiter
     end 
   end 
   
-  def best_tipper
+ef best_tipper
   best_tipped_meal = meals.max do |meal_a, meal_b|
     meal_a.tip <=> meal_b.tip
-    end 
   end
+ 
+  best_tipped_meal.customer
+end
 end 
